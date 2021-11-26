ff652aacb43b47a077bd03399631c8fd2eab897a
Ján Kubaška <jajanokubaska@gmail.com>
2021-11-26 08:25:45 +0100

    Zmena na 10.01

--- Open-Lab-10.00/Program.cs
+++ Open-Lab-10.00/Program.cs
@@ -3,10 +3,19 @@
 namespace Open_Lab_10._00
 {
     class Program
+        
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Book LOTR = new Book();
+
+            LOTR.title = "LOTR";
+            Console.WriteLine (LOTR.title);
         }
+    }
+    class Book
+    {
+        public string title;
+        private int pages;
     }
 }
