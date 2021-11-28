f1e7754389fb20d838dc0dded79532fd65f7bdd1
Ján Kubaška <jajanokubaska@gmail.com>
2021-11-28 12:31:04 +0100

    10.02.

--- Open-Lab-10.00/Program.cs
+++ Open-Lab-10.00/Program.cs
@@ -8,14 +8,51 @@ namespace Open_Lab_10._00
         static void Main(string[] args)
         {
             Book LOTR = new Book();
+            LOTR.Author("Zbyszek");
+            LOTR.Category("Fantasy");
+            LOTR.ReleaseDate(2030);
+            LOTR.Title("´FrostAge");
+            LOTR.Pages(1200);
+            LOTR.Returner();
 
-            LOTR.title = "LOTR";
-            Console.WriteLine (LOTR.title);
+
         }
     }
     class Book
     {
-        public string title;
+        private string title;
         private int pages;
+        private string author;
+        private string category;
+        private int releaseDate;
+        public void Title(string gettitle)
+        {
+            title = gettitle;
+        }
+        public void Pages(int getpages)
+        {
+            pages = getpages;
+        }
+        public void Author(string getauthor)
+        {
+            author = getauthor;
+        }
+        public void ReleaseDate(int getreleaseDate)
+        {
+            releaseDate = getreleaseDate;
+        }
+        public void Category (string getcategory)
+        {
+            category = getcategory;
+        }
+        public void Returner()
+        {
+            Console.WriteLine(title);
+            Console.WriteLine(pages);
+            Console.WriteLine(author);
+            Console.WriteLine(releaseDate);
+            Console.WriteLine(category);
+
+        }
     }
 }
